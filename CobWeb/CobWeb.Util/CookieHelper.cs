417094ab46c6dceff6ce53b15e4dcce7b2104391
amayergogh <454593034@qq.com>
2019-04-14 22:52:58 +0800

    Create CookieHelper.cs

---
+++ CobWeb/CobWeb.Util/CookieHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobWeb.Util
+{
+    public  class CookieHelper
+    {
+    }
+    /// <summary>
+    /// cookie伪类
+    /// </summary>
+    public class CookiePseudo
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Domain { get; set; }
+        public string Path { get; set; }
+        public bool Secure { get; set; }
+        public bool HttpOnly { get; set; }
+        public DateTime? Expires { get; set; }
+        public DateTime Creation { get; set; }
+        public DateTime LastAccess { get; set; }
+    }
+}
